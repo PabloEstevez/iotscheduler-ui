c2b819eb6b145edf4d0a032cb40e4b2cc76cd8aa
Caesar-Hackiit <48597453+Caesar-Hackiit@users.noreply.github.com>
2020-12-30 17:32:34 +0100

    incluido componentes gráficos: calendario, slide y checkbox

--- src/App.js
+++ src/App.js
@@ -3,10 +3,46 @@ import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import Switch from '@material-ui/core/Switch';
+import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@material-ui/core/Container';
 
+//imports para el calendario
+import 'date-fns';
+import Grid from '@material-ui/core/Grid';
+import DateFnsUtils from '@date-io/date-fns';
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from '@material-ui/pickers';
+//imports para el slider
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Slider from '@material-ui/core/Slider';
 
 function App() {
   
+  //parametros para el slider
+  const useStyles = makeStyles({
+    root: {
+      width: 300,
+    },
+  });
+  
+  function valuetext(value) {
+    return `${value}°C`;
+  }
+  
+
+  //parametros para el calendario
+  const [selectedDate, setSelectedDate] = React.useState(new Date('2021-01-01T21:01:01'));
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  //parametros para el switch
+
   const [state, setState] = React.useState({
     checked: true,
   });
@@ -63,8 +99,66 @@ function App() {
       }}>
           Hello World
       </Button>
+
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container justify="space-around">
+          <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="dd/MM/yyyy"
+            margin="normal"
+            id="date-picker-inline"
+            label="Fecha del riego"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change date',
+            }}
+          />
+          <KeyboardTimePicker
+            margin="normal"
+            id="time-picker"
+            label="Hora de inicio del riego"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              'aria-label': 'change time',
+            }}
+          />
+        </Grid>
+      </MuiPickersUtilsProvider>
+
+      <Typography id="discrete-slider" gutterBottom>
+        Duración del riego
+      </Typography>
+      <Slider
+        defaultValue={30}
+        getAriaValueText={valuetext}
+        aria-labelledby="discrete-slider"
+        valueLabelDisplay="auto"
+        step={10}
+        marks
+        min={10}
+        max={110}
+      />
+
+      <Container fixed>
+        <Checkbox
+          defaultChecked
+          color="primary"
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
+        <Typography id="discrete-slider" gutterBottom>
+          Repetir de manera periódica este riego
+        </Typography>
+      </Container>
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
