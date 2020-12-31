0b4cf3456f383e75fbda64b05d569230149ae60d
PabloEstevez <pabloestevegonzalez@gmail.com>
2020-12-31 08:53:53 +0100

    Intento de on_off

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button';
 import Switch from '@material-ui/core/Switch';
@@ -21,18 +21,26 @@ import Typography from '@material-ui/core/Typography';
 import Slider from '@material-ui/core/Slider';
 
 function App() {
-  
+
+  // VARIABLES GLOBALES
+  const API_URL = "http://192.168.0.129:5000";
+  const requestOptions_GET = {
+    method: 'GET',
+    headers: { 'Content-Type': 'application/json' }
+  };
+
+
   //parametros para el slider
-  const useStyles = makeStyles({
-    root: {
-      width: 300,
+  const classes = makeStyles({
+    slider: {
+      width: 50,
     },
   });
-  
+
   function valuetext(value) {
-    return `${value}°C`;
+    return `${value} minutos`;
   }
-  
+
 
   //parametros para el calendario
   const [selectedDate, setSelectedDate] = React.useState(new Date('2021-01-01T21:01:01'));
@@ -44,112 +52,117 @@ function App() {
   //parametros para el switch
 
   const [state, setState] = React.useState({
-    checked: true,
+    checked: false,
   });
 
-  const handleChange = (event) => {
+  // FUNCIONES
+
+  const handleSwitchChange = async (event) => {
     //setState({ ...state, [event.target.name]: event.target.checked });
-    let id_zona = 'huerto';
-    let nuevoEstado;
+    let id_sector = 'huerto';
+    //let nuevoEstado;
 
     //Petición GET a gpio_status para obtener el estado del riego
-    const requestOptions_GET = {
-      method: 'GET',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ id: id_zona })
-    };
-
-    fetch('http://rapsberry.local:5000/gpio_status')
-        .then(response => response.json())
-        .then(json => {
-          console.log(json);
-          nuevoEstado= !json;
-        });
-    
+    const estado = await fetch(`${API_URL}/gpio_status?id=${encodeURIComponent(id_sector)}`, requestOptions_GET)
+      .then(response => response.json().gpio_status);
+      /*.then(json => {
+        nuevoEstado = !json.gpio_status;
+        console.log("Nuevo estado: " + nuevoEstado);
+      });*/
     
     //Petición POST a on_off para cambiar el estado del riego
-    const requestOptions_POST = {
+    const requestOptions_POST = await {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ id: id_zona, status: nuevoEstado })
-    };    
+      body: JSON.stringify({ "id": id_sector, "status": bool2string(!estado) })
+    };
 
-    fetch('http://rapsberry.local:5000/on_off',requestOptions_POST)
-        .then(response => response.json())
-        .then(json => console.log(json)).then(setState({ ...state, checked: nuevoEstado }));
-    
-    
-
+    await fetch(`${API_URL}/on_off`, requestOptions_POST)
+      .then(response => response.json())
+      .then(json => console.log(json)).then(setState({ ...state, checked: !estado })).then(console.log("Estado: " + estado));
   };
+
+  function bool2string(b){
+    return b ? "1" : "0"; 
+  }
 
   return (
     <div className="App">
-      <Switch
-        checked={state.checked}
-        onChange={handleChange}
-        color="primary"
-        name="checked"
-        inputProps={{ 'aria-label': 'primary checkbox' }}
-      />
+      <Container fixed>
+        <Switch
+          checked={state.checked}
+          onChange={handleSwitchChange}
+          color="primary"
+          name="checked"
+          inputProps={{ 'aria-label': 'primary checkbox' }}
+        />
 
-      <Button variant="contained" color="primary" onClick={() => {
-        fetch('https://jsonplaceholder.typicode.com/todos/1')
-        .then(response => response.json())
-        .then(json => console.log(json)).then(console.log("illo funciona"));
-      }}>
-          Hello World
+        <Button variant="contained" color="primary" onClick={() => {
+          let id_sector = 'huerto';
+          let nuevoEstado = 0;
+          const requestOptions_POST = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ "id": id_sector, "status": nuevoEstado.toString() })
+          };
+
+          fetch(`${API_URL}/on_off`, requestOptions_POST)
+            .then(response => response.json())
+            .then(json => console.log(json));
+        }}>
+          Prueba
       </Button>
 
-      <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        <Grid container justify="space-around">
-          <KeyboardDatePicker
-            disableToolbar
-            variant="inline"
-            format="dd/MM/yyyy"
-            margin="normal"
-            id="date-picker-inline"
-            label="Fecha del riego"
-            value={selectedDate}
-            onChange={handleDateChange}
-            KeyboardButtonProps={{
-              'aria-label': 'change date',
-            }}
-          />
-          <KeyboardTimePicker
-            margin="normal"
-            id="time-picker"
-            label="Hora de inicio del riego"
-            value={selectedDate}
-            onChange={handleDateChange}
-            KeyboardButtonProps={{
-              'aria-label': 'change time',
-            }}
-          />
-        </Grid>
-      </MuiPickersUtilsProvider>
+        <MuiPickersUtilsProvider utils={DateFnsUtils}>
+          <Grid container justify="space-around">
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              format="dd/MM/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="Fecha del riego"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change date',
+              }}
+            />
+            <KeyboardTimePicker
+              margin="normal"
+              id="time-picker"
+              label="Hora de inicio del riego"
+              value={selectedDate}
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                'aria-label': 'change time',
+              }}
+            />
+          </Grid>
+        </MuiPickersUtilsProvider>
 
-      <Typography id="discrete-slider" gutterBottom>
-        Duración del riego
-      </Typography>
-      <Slider
-        defaultValue={30}
-        getAriaValueText={valuetext}
-        aria-labelledby="discrete-slider"
-        valueLabelDisplay="auto"
-        step={10}
-        marks
-        min={10}
-        max={110}
-      />
-
-      <Container fixed>
+        <Typography id="discrete-slider" gutterBottom>
+          Duración
+        </Typography>
+        <Slider
+          margin="normal"
+          className={classes.slider}
+          defaultValue={30}
+          getAriaValueText={valuetext}
+          aria-labelledby="discrete-slider"
+          valueLabelDisplay="auto"
+          step={5}
+          marks
+          min={5}
+          max={180}
+        />
         <Checkbox
           defaultChecked
           color="primary"
           inputProps={{ 'aria-label': 'secondary checkbox' }}
         />
         <Typography id="discrete-slider" gutterBottom>
-          Repetir de manera periódica este riego
+          Repetir de manera periódica
         </Typography>
       </Container>
 
